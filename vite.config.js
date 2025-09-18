c7e168f1e0d235fbad1fba13c48cd7ffefa3fffd
LeonardoCandidoMartins <leonardocandidomartins.inf@gmail.com>
2025-09-18 11:37:58 -0300

    Convercao do projeto para uma PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,55 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'], // pode trocar por seus ícones
+      manifest: {
+        name: 'OftalmoApp Auxilio Triagem',
+        short_name: 'OftalmoApp',
+        description: 'Projeto de TCC de Leonardo Candido Martins, aplicacao para axilio em triagens oftalmologicas',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.pathname.startsWith('/data/'),
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'json-cache',
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // 7 dias
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 })
