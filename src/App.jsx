6bf14b9a6a7ed47157bdf2ca61d04276de88e0bf
LeonardoCandidoMartins <leonardocandidomartins.inf@gmail.com>
2025-09-19 17:20:45 -0300

    rola para o topo ao trocar de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 //nao irei importar mais estilização, pois não será mais uma pagina, ele apenas importa do index.jsx
 import AppRoutes from '../routes';
+import ScrollToTop from '../routes/ScrollToTop';
 
 function App() {
-  return(
-    <AppRoutes />
-  );  
+  return (
+    <>
+      <ScrollToTop />
+      <AppRoutes />
+    </>
+  );
 }
 
-export default App
+export default App;
